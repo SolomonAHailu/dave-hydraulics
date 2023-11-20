2850c9c3d1a17a9a517325129e23bf7ca276d871
SolomonAHailu <solomonasregdew1992@gmail.com>
2023-11-20 12:28:18 +0300

    small changes on the  pictures of partners

--- src/data/teamData/index.js
+++ src/data/teamData/index.js
@@ -1,47 +1,26 @@
+import DaveImg from "./images/dave.jpg";
+import EndexImg from "./images/endex.jpg";
+import MulusewImg from "./images/mulusew.jpg";
+
 const teamData = [
   {
-    name: "John Doe",
+    name: "David Asregdew",
     position: "CEO",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "A Water Engineer at Debre Markos University",
+    imageUrl: DaveImg,
   },
   {
-    name: "Jane Smith",
-    position: "CTO",
-    bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Endedilu Shumet",
+    position: "Partner",
+    bio: "A Mechanical Engineer at Bahir Dar University",
+    imageUrl: EndexImg,
   },
   {
-    name: "John Doe",
-    position: "CEO",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Mulusew Zegeye",
+    position: "Partner",
+    bio: "A Water Engineer.",
+    imageUrl: MulusewImg,
   },
-  {
-    name: "Jane Smith",
-    position: "CTO",
-    bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "John Doe",
-    position: "CEO",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Jane Smith",
-    position: "CTO",
-    bio: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  // Add more team members as needed
 ];
 
 export default teamData;
