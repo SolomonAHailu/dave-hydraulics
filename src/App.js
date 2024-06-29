dfb70f29fdad601235f117ea3a228cd32a7cf101
Solomon Asregdew <117452698+SolomonAHailu@users.noreply.github.com>
2024-06-30 01:28:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
